eb4fd271a6ed31023fbef00cc5052dfaf3be9153
Deadinsidek <dizexen@gmail.com>
2022-04-05 13:33:23 +0300

    Add lab5

---
+++ Lab5_2semestr/src/main/java/Main.java
@@ -0,0 +1,20 @@
+import java.io.File;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.run();
+
+    }
+    void run() {
+        ArrayReader arrayReader = new ArrayReader();
+        ArrayTasks arrayTasks = new ArrayTasks();
+        arrayTasks.processArray(arrayReader.readTwoDimensionalArray("lab52.txt"));
+
+
+    }
+
+
+}
+
