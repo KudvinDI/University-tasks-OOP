a84efe3bf50b783cafa9466ccadc466dafa1ef04
Deadinsidek <dizexen@gmail.com>
2022-05-24 18:22:59 +0300

    ?

--- Lab5_2semestr/src/main/java/ArrayProcessor.java
+++
@@ -1,9 +0,0 @@
-public interface ArrayProcessor {
-    double calculate(double[] array);
-
-    double calculate(double[][] array);
-
-    void processArray(double[] array);
-
-    void processArray(double[][] array);
-}
